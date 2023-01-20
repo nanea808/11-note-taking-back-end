67b605950d1bd1d3e4c0d7eb2073805e85419518
Ethan Willers <willers.enw@gmail.com>
2023-01-19 18:16:50 -0800

    POST and GET routes for api/notes

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
 
+const routes = require('./routes/index');
+
 const PORT = 3001;
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use('/api', routes);
 
 app.use(express.static('public'));
 
