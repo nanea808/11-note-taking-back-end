b6ba8258d91939cb2df29d8836d7f88bdfc09f1f
Ethan Willers <willers.enw@gmail.com>
2023-01-22 20:35:29 -0800

    route comments

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,11 +1,13 @@
 const notesRouter = require('express').Router();
 const { readFromFile } = require('../helpers/fsUtils');
 
+// Get route
 notesRouter.get('/', (req, res) => {
     console.info(`${req.method} request received for /notes`);
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+// Post route
 notesRouter.post('/', (req, res) => {
     console.info(`${req.method} request received for /notes`);
     res.json('POST request sent to /notes');
